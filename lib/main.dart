33b4a1a51924d844b7f704f3b15340f11fd109a4
Tuhin <tuhinroy881@gmail.com>
2023-02-14 17:24:43 +0530

    bug fix 3

--- lib/main.dart
+++ lib/main.dart
@@ -40,22 +40,19 @@ class _MyHomePageState extends State<MyHomePage> {
     _initSpeech();
   }
 
-  /// This has to happen only once per app
+
   void _initSpeech() async {
     _speechEnabled = await _speechToText.initialize();
     setState(() {});
   }
 
-  /// Each time to start a speech recognition session
+ 
   void _startListening() async {
     await _speechToText.listen(onResult: _onSpeechResult);
     setState(() {});
   }
 
-  /// Manually stop the active speech recognition session
-  /// Note that there are also timeouts that each platform enforces
-  /// and the SpeechToText plugin supports setting timeouts on the
-  /// listen method.
+ 
   void _stopListening() async {
     await _speechToText.stop();
     setState(() {});
@@ -65,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
   /// the platform returns recognized words.
   void _onSpeechResult(SpeechRecognitionResult result) {
     setState(() {
-     _lastWords = result.recognizedWords; // concatenate new text with previous text
+     _lastWords = result.recognizedWords; 
        _textController.text = _lastWords;
     });
   }
